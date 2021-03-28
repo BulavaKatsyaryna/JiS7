d79793a38bee6319adc702cfb3e48330db84bc08
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-03-28 19:00:28 +0300

    Add files via upload

---
+++ Lecture_2/Heads and Tails/src/main/java/HeadsAndTails.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class HeadsAndTails {
+    public static void main( String args[] ) {
+
+        Random random = new Random();
+
+        boolean value = random.nextBoolean();
+
+        System.out.println("Value is: " + value);
+    }
+}
+
+//    Второй метод:
+//    public static boolean getRandomBoolean() {
+//        return Math.random() < 0.5;
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(getRandomBoolean());
+//    }
+//}
+
