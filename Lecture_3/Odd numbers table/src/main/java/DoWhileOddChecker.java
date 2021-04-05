0bdb40726ccb7f06cf12640c87162f55c20e78dc
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2021-04-05 22:38:33 +0300

    Update DoWhileOddChecker.java
    //***********************Checked***********************

--- Lecture_3/Odd numbers table/src/main/java/DoWhileOddChecker.java
+++ Lecture_3/Odd numbers table/src/main/java/DoWhileOddChecker.java
@@ -1,10 +1,10 @@
 public class DoWhileOddChecker {
-
+//***********************Checked***********************
     public static void main(String args[]) {
         int i=0;
         do {
             i++;
-            if(i % 2==1)
+            if(i % 2==1) // Or i % 2 != 0 (the same)
                 System.out.println(i);
         } while (i < 50);
     }
