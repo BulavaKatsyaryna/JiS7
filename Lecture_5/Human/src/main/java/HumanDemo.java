899ebf3d747f706a9dad7ec581567d6a58c86a1f
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-04-07 15:01:29 +0300

    Add files via upload

---
+++ Lecture_5/Human/src/main/java/HumanDemo.java
@@ -0,0 +1,12 @@
+public class HumanDemo {
+
+    public static void main(String[] args) {
+        Human people = new Human();
+
+        people.name = "Katya";
+        people.age = 26;
+
+        System.out.println("Hi! My name is " + people.name);
+        System.out.println("I'm " + people.age + " years old");
+    }
+}
