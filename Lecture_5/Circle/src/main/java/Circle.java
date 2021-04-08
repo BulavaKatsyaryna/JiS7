a92b72ba6d2b5cbb2adbaeecb89c213703bf7853
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-04-08 23:19:38 +0300

    Add files via upload

---
+++ Lecture_5/Circle/src/main/java/Circle.java
@@ -0,0 +1,8 @@
+public class Circle {
+
+    public double radius;
+
+    public double calculateArea() {
+        return 3.14 * Math.pow(radius, 2);
+    }
+}
---
+++ Lecture_5/Circle/src/main/java/CircleDemo.java
@@ -0,0 +1,11 @@
+public class CircleDemo {
+
+    public static void main(String[] args) {
+
+        Circle circle = new Circle();
+
+        circle.radius = 18;
+        double i = circle.calculateArea();
+        System.out.println("The area of the circle is: " + circle.calculateArea());
+    }
+}
