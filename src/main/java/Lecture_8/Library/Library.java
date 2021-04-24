a8644a29aa1af508889f4de7e421832fc680b348
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-04-25 01:03:34 +0300

    HW lecture 8, Library - done

--- src/main/java/Lecture_8/Library/Library.java
+++ src/main/java/Lecture_8/Library/Library.java
@@ -1,4 +1,35 @@
 package Lecture_8.Library;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+import java.util.*;
+
 public class Library {
+
+    ListMultimap<Writer, String> library = ArrayListMultimap.create();
+
+    String name;
+
+    public void addBook(Writer writer, String name) {
+        this.name = name;
+        library.put(writer, name);
+    }
+
+    public void bookByAuthor() {
+        List<String> bookByAuthor = library.get(Writer.CRONIN);
+        System.out.println("Book by author Archibald Joseph Cronin: " + bookByAuthor);
+    }
+
+    public void deletingBook() {
+        library.remove(Writer.ROWLING, "Harry Potter and the Philosopher's Stone (MACHAON)");
+        System.out.println("library = " + library);
+    }
+
+    public void titleOfTheBook() {
+        for (Map.Entry<Writer, String> entry : library.entries()) {
+            if (entry.getValue().equals("The Mists of Avalon")) {
+                System.out.println("The Mists of Avalon, wrote: " + entry.getKey());
+            }
+        }
+    }
 }
--- src/main/java/Lecture_8/Library/Writer.java
+++ src/main/java/Lecture_8/Library/Writer.java
@@ -1,12 +1,12 @@
 package Lecture_8.Library;
 
-public enum Book {
+public enum Writer {
     ROWLING("J. K. Rowling"), BRADLEY("Marion Zimmer Bradley"), ROTHFUSS("Patrick Rothfuss"), CRONIN("Archibald Joseph Cronin");
 
     private final String typeWriter;
 
-    Book(String usersType) {
-        this.typeWriter = usersType;
+    Writer(String writersType) {
+        this.typeWriter = writersType;
     }
     public String getTypeWriter() {
         return typeWriter;
