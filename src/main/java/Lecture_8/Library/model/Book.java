a82d462959d3b85d4e503c44a2a62b06e2f97236
Artsemi.Stankevich <articzander@mail.ru>
2021-05-01 11:43:35 +0300

    refactor by Artsemi

---
+++ src/main/java/Lecture_8/Library/model/Book.java
@@ -0,0 +1,13 @@
+package Lecture_8.Library.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String authorName;
+    private String bookName;
+}
