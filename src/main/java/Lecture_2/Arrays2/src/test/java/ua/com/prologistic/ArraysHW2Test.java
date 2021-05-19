613e2bff3abc8b44920fadb62292fec6360b95e7
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-05-20 00:43:13 +0300

    Stream random numbers is done

--- src/main/java/Lecture_2/Arrays2/src/test/java/ua/com/prologistic/ArraysHW2Test.java
+++
@@ -1,6 +0,0 @@
-package Lecture_2.Arrays2.src.test.java.ua.com.prologistic;
-import static org.junit.jupiter.api.Assertions.*;
-
-class ArraysHW2Test {
-
-}
---
+++ src/main/java/Lecture_5/HumanDemo.java
@@ -0,0 +1,16 @@
+package Lecture_5;
+
+import Lecture_5_6.Human.Human;
+
+public class HumanDemo {
+
+    public static void main(String[] args) {
+        Human people = new Human();
+
+        people.name = "Katya";
+        people.age = 26;
+
+        System.out.println("Hi! My name is " + people.name);
+        System.out.println("I'm " + people.age + " years old");
+    }
+}
