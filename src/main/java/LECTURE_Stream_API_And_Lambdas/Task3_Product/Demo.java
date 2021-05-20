6141aaadbd2fe572f8e04e2e2b4dc70a28f3410f
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-05-20 15:39:29 +0300

    initial commit

---
+++ src/main/java/LECTURE_Stream_API_And_Lambdas/Task3_Product/Demo.java
@@ -0,0 +1,4 @@
+package LECTURE_Stream_API_And_Lambdas.Task3_Product;
+
+public class Demo {
+}
---
+++ src/main/java/LECTURE_Stream_API_And_Lambdas/Task3_Product/Product.java
@@ -0,0 +1,16 @@
+package LECTURE_Stream_API_And_Lambdas.Task3_Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    String name;
+    Integer prise;
+    Integer discount;
+
+}
