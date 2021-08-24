09e59de3be0acdff655fdd72dc0542d1dd9bf2fa
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-08-24 03:30:02 +0300

    Spring Container configuration using XML file

---
+++ src/main/java/spring_course/src/main/java/org/example/Test2.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Pet pet = ctx.getBean("myDog", Pet.class);
+        pet.say();
+        ctx.close();
+    }
+}
