6cb2a8e780ff77a3b3dee9c079fb0882bcd09a3d
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-09-04 00:01:59 +0300

    Dependency injection with Autowired annotation via setter

--- src/main/java/spring_course/src/main/java/org/example/Person.java
+++ src/main/java/spring_course/src/main/java/org/example/Person.java
@@ -2,6 +2,7 @@ package org.example;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,32 +14,25 @@ public class Person {
     private String surname;
     private int age;
 
-    @Autowired
-    public Person(Pet pet) {
-        System.out.println("Person bean is created");
-        this.pet = pet;
-    }
-
-//    public Person() {
+//    @Autowired
+//    public Person(Pet pet) {
 //        System.out.println("Person bean is created");
+//        this.pet = pet;
 //    }
 
+    public Person() {
+        System.out.println("Person bean is created");
+    }
+
+    @Autowired
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
         this.pet = pet;
     }
 
-    public String getSurname() {
-        return surname;
-    }
-
     public void setSurname(String surname) {
         System.out.println("Class Person: set surname");
         this.surname = surname;
-    }
-
-    public int getAge() {
-        return age;
     }
 
     public void setAge(int age) {
